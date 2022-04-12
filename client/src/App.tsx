0d0caa9065a388021f0e3d3aa20920d0227c5055
Alan Shiau <alanshiauwork@hotmail.com>
2022-04-12 22:00:02 +1000

    revmoed unused auth middleware

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,21 @@
-import { Component, useEffect} from "react";
+import { Component, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./custom.scss";
 // import "./App.css";
-import { Switch, Route, useHistory} from "react-router-dom";
+import { Switch, Route, useHistory } from "react-router-dom";
 import LoginPage from "./components/login-page";
 import ForgotPwd from "./components/forgot-password";
 import Wrapper from "./components/wrappers/verification_wrapper";
 import MainView from "./components/wrappers/main_view_wrapper";
 import SignUp from "./components/sign-up";
-import Calendar from "./components/test.component"
+import Calendar from "./components/test.component";
 import PrivateRoute from "./components/wrappers/ProtectedRouteWrapper";
 import { connect, useDispatch, useSelector } from "react-redux";
 import {
   userHasAuthenticated,
   userIsAuthenticating,
 } from "./redux/reducers/userSessionSlice";
-import TestFunctionalComponent from "./components/testFuncationalComponent"
+import TestFunctionalComponent from "./components/testFuncationalComponent";
 import UserAccessService from "./services/user.access";
 import { store } from "./redux/store";
 import { IsValidUserSessionDocument } from "./generated";
@@ -28,32 +28,37 @@ interface Props {
   userIsAuthenticating: any;
 }
 
-
-
-export function AppFunctional(){
-  const {loading, error, data} = useQuery(IsValidUserSessionDocument)
-  const isAuthenticating = useSelector((state: any) => state.usersession.isAuthenticating)
-  const isAuthenticated = useSelector((state: any) => state.usersession.isAuthenticated)
-  const history = useHistory()
-  const dispatch = useDispatch()
+export function AppFunctional() {
+  const { loading, error, data } = useQuery(
+    IsValidUserSessionDocument,
+  );
+  const isAuthenticating = useSelector(
+    (state: any) => state.usersession.isAuthenticating,
+  );
+  const isAuthenticated = useSelector(
+    (state: any) => state.usersession.isAuthenticated,
+  );
+  const history = useHistory();
+  const dispatch = useDispatch();
   useEffect(() => {
-    if(data){
-      if(data.isValidUserSession){
-        console.log('is a valid session setting userHasUAuthenticate to true')
-        dispatch(userHasAuthenticated(true))
+    if (data) {
+      if (data.isValidUserSession) {
+        console.log(
+          "is a valid session setting userHasUAuthenticate to true",
+        );
+        dispatch(userHasAuthenticated(true));
+      } else {
+        console.log("is not a valid session");
+        dispatch(userHasAuthenticated(false));
       }
-      else{
-        console.log("is not a valid session")
-        dispatch(userHasAuthenticated(false))
+      console.log("setting is authenticating to false");
+      dispatch(userIsAuthenticating(false));
+      if (isAuthenticated == true) {
+        history.push("/main");
       }
-      console.log("setting is authenticating to false")
-      dispatch(userIsAuthenticating(false))
-      if(isAuthenticated==true){
-        history.push("/main")
-      }
-  }
-  })
-  if(!isAuthenticating) {
+    }
+  });
+  if (!isAuthenticating) {
     return (
       <div className="App">
         <div>
@@ -70,36 +75,36 @@ export function AppFunctional(){
             />
             <Route exact path="/signup" component={Wrapper(SignUp)} />
             <PrivateRoute
-            path='/main'
-            // isAuthenticated={this.props.isAuthenticated}
-            component={MainView}
-        />
+              path="/main"
+              // isAuthenticated={this.props.isAuthenticated}
+              component={MainView}
+            />
             <Route path="/calendar" component={Calendar} />
             <Route path="/test" component={TestFunctionalComponent} />
           </Switch>
         </div>
       </div>
-    
-      )
+    );
   }
-  return null
+  return null;
 }
 
 class App extends Component<Props> {
   async onLoad() {
-    console.log("hit onload")
-    const isValidSession = await UserAccessService.isCurrentSessionValid()
-    if(isValidSession){
-      console.log('is a valid session setting userHasUAuthenticate to true')
-      store.dispatch(userHasAuthenticated(true))
+    console.log("hit onload");
+    const isValidSession =
+      await UserAccessService.isCurrentSessionValid();
+    if (isValidSession) {
+      console.log(
+        "is a valid session setting userHasAuthenticate to true",
+      );
+      store.dispatch(userHasAuthenticated(true));
+    } else {
+      console.log("is not a valid session");
+      store.dispatch(userHasAuthenticated(false));
     }
-    else{
-      console.log("is not a valid session")
-      store.dispatch(userHasAuthenticated(false))
-    }
-    console.log("setting is authenticating to false")
-    store.dispatch(userIsAuthenticating(false))
-    
+    console.log("setting is authenticating to false");
+    store.dispatch(userIsAuthenticating(false));
   }
   componentDidMount() {
     this.onLoad();
@@ -107,43 +112,44 @@ class App extends Component<Props> {
   render() {
     return (
       !this.props.isAuthenticating && (
-      <div className="App">
-        <div>
-          <Switch>
-            <Route
-              exact
-              path={["/"]}
-              component={Wrapper(LoginPage)}
-            />
-            <Route
-              exact
-              path="/forgot"
-              component={Wrapper(ForgotPwd)}
-            />
-            <Route exact path="/signup" component={Wrapper(SignUp)} />
-            <PrivateRoute
-            path='/main'
-            // isAuthenticated={this.props.isAuthenticated}
-            component={MainView}
-        />
-            <Route path="/calendar" component={Calendar} />
-            <Route path="/test" component={TestFunctionalComponent} />
-          </Switch>
+        <div className="App">
+          <div>
+            <Switch>
+              <Route
+                exact
+                path={["/"]}
+                component={Wrapper(LoginPage)}
+              />
+              <Route
+                exact
+                path="/forgot"
+                component={Wrapper(ForgotPwd)}
+              />
+              <Route
+                exact
+                path="/signup"
+                component={Wrapper(SignUp)}
+              />
+              <PrivateRoute path="/main" component={MainView} />
+              <Route path="/calendar" component={Calendar} />
+              <Route
+                path="/test"
+                component={TestFunctionalComponent}
+              />
+            </Switch>
+          </div>
         </div>
-      </div>
-    
       )
-    )
+    );
   }
 }
 
 const mapStateToProps = (state: any) => ({
-    isAuthenticating: state.usersession.isAuthenticating,
-    isAuthenticated: state.usersession.isAuthenticated,
+  isAuthenticating: state.usersession.isAuthenticating,
+  isAuthenticated: state.usersession.isAuthenticated,
 });
 
 export default connect(mapStateToProps, {
   userHasAuthenticated,
   userIsAuthenticating,
 })(App);
-
--- server/middleware/authentication.ts
+++ server/middleware/authentication.ts
@@ -1,123 +1,9 @@
 import * as express from "express";
-// import { UserSessionMod } from "../../node/mongo/entities/userSession";
 import {UserService} from "../services/user-service"
 import * as jwt from "jsonwebtoken";
-import { AuthError } from "../common/auth-error";
-import { HttpStatusCode } from "../common/http-status-code";
-// import { AuthenticationError } from "apollo-server";
 
-// declare module "jsonwebtoken" {
-//   export interface JwtPayload {
-//     sessionId: string,
-//     userId: string
-//   }
-// }
-export async function expressAuthentication(
-  request: express.Request,
-  securityName: string,
-  scopes?: string[]
-): Promise<any> {
-    console.log(scopes)
-  console.log("security name", securityName)
-  if(securityName === "api_key"){    
-    try {
-      const userService = new UserService();
-      let jwtToken = request.cookies['token']
-      // console.log("jwtToken", jwtToken)
-      const decodedToken = jwt.verify(jwtToken,process.env.JWT_SECRET!)
-      if (typeof decodedToken === 'string' || decodedToken instanceof String) {
-        throw new AuthError("AUTHENTICATION_ERROR", HttpStatusCode.UNAUTHORIZED)
-      }
-      else{     
-        console.log("Hit decoded string") 
-        let isValidUserSession = await userService.isValidUserSession(decodedToken.sessionId, decodedToken.userId)
-        if (isValidUserSession){
-          console.log('resolving promise')
-          return Promise.resolve({
-            userId: decodedToken.userId,
-            sessionId: decodedToken.sessionId
-          })
-
-        }
-        throw new AuthError("INVALID_SESSION", HttpStatusCode.UNAUTHORIZED)
-      }
-    }catch{
-      throw new AuthError("AUTHENTICATION_ERROR", HttpStatusCode.UNAUTHORIZED)
-    }
-  
-  }
-  return Promise.reject({})
-
-
-//   if (securityName === "jwt") {
-//     const token =
-//       request.body.token ||
-//       request.query.token ||
-//       request.headers["x-access-token"];
-
-//     return new Promise((resolve, reject) => {
-//       if (!token) {
-//         reject(new Error("No token provided"));
-//       }
-//       jwt.verify(token, "[secret]", function (err: any, decoded: any) {
-//         if (err) {
-//           reject(err);
-//         } else {
-//           // Check if JWT contains all required scopes
-//           for (let scope of scopes) {
-//             if (!decoded.scopes.includes(scope)) {
-//               reject(new Error("JWT does not contain required scope."));
-//             }
-//           }
-//           resolve(decoded);
-//         }
-//       });
-//     });
-//   }
-}
-
-
-// export async test(req: express.Request) => {
-//   console.log("Hit graphql authentication")
-//   // return {user: "testuser"}
-//   try {
-//     const userService = new UserService();
-//     console.log("cookies",req.cookies)
-//     let jwtToken = req.cookies['token']
-//     console.log("jwtToken", jwtToken)
-//     const decodedToken = jwt.verify(jwtToken,process.env.JWT_SECRET!)
-//     if (typeof decodedToken === 'string' || decodedToken instanceof String) {
-//       return {user: "testUser"}
-//       // return null
-//       // throw new AuthenticationError("Authentication Error")
-//     }
-//     else{     
-//       console.log("Hit decoded string") 
-//       let isValidUserSession = await userService.isValidUserSession(decodedToken.sessionId, decodedToken.userId)
-//       if (isValidUserSession){
-//         console.log('resolving promise')
-
-//         return Promise.resolve({
-//           userId: decodedToken.userId,
-//           sessionId: decodedToken.sessionId,
-//           isValidSession: true
-//         })
-
-//       }
-//       return {user: "testUser"}
-//       // return null
-//       // throw new AuthenticationError("You Must Be Logged In")
-//     }
-//   }catch(err){
-//     console.log(err)
-//     return {user: "testUser"}
-//     // return null
-//     // throw new AuthenticationError("Authentication Error")
-//   }
-// }
 
 export default async (req: any, _: express.Response, next: express.NextFunction) => {
-  // console.debug("hit auth middleware")
   try {
     const userService = new UserService();
     const decodedToken = validateAndDecodeJwtToken(req.cookies['token'])
@@ -130,8 +16,8 @@ export default async (req: any, _: express.Response, next: express.NextFunction)
     }
   } catch(err) {
       // console.debug(err)
-      req.userId = 1
-      req.sessionId = 1
+      req.userId = null
+      req.sessionId = null
   }
   return next();
 }
